fdf851e6f09760d9a853d4ed8f2ca25bcdf4b52c
Danila Rogozhnikov <100974943+12082000Dr@users.noreply.github.com>
2022-03-10 11:57:16 +0400

    Delete .idea directory

--- .idea/ghghgjghgjhg.py
+++
@@ -1,3 +0,0 @@
-def count_letters(a):
-    print(f'Количество заглавных символов: {len([i for i in a if i.isupper()])}')
-    print(f'Количество строчных символов: {len([i for i in a if i.islower()])}')
